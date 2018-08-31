666f7440831d2b2b1353a1e95a88a2a82298c8aa
perrinprograms <phawver@icloud.com>
2018-08-31 17:13:35 -0400

    users can add a username

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-var app = require('express')();
+const express = require('express');
+var app = express()
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var usernames = [];
+
+
+
+app.use(express.static(__dirname));
+
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/chat.html');
 });
 
 io.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
+
+    socket.on('join', function (name) {
+        socket.name = name;
+        usernames.push(socket.name);
+        io.emit('join', socket.name);
+    });
+
+    socket.on('chat message', function (data) {
+        io.emit('chat message', {
+            msg: data,
+            name: socket.name
+        });
+    });
+    socket.on('disconnect', function (data) {
+        io.emit('leave', {
+            name: socket.name
+        });
     });
 });
 
