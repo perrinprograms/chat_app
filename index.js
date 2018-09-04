7722178c2d6c7d8d9ce09ce575f6f7a186ef5cee
Perrin Hawver <perrinhawver@Perrins-MacBook.local>
2018-09-03 20:18:31 -0400

    added index.js

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
-const express = require('express');
-var app = express()
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
 var usernames = [];
 
-
-
 app.use(express.static(__dirname));
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/chat.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/chat.html");
 });
 
-io.on('connection', function (socket) {
+io.on("connection", function(socket) {
+  socket.on("join", function(name) {
+    socket.name = name;
+    usernames.push(socket.name);
+    io.emit("join", socket.name);
+  });
 
-    socket.on('join', function (name) {
-        socket.name = name;
-        usernames.push(socket.name);
-        io.emit('join', socket.name);
-    });
+  socket.on("user typing", function(isUserTyping) {
+    socket.typing = isUserTyping;
+    io.emit("user typing", { name: socket.name, typing: socket.typing });
+  });
 
-    socket.on('chat message', function (data) {
-        io.emit('chat message', {
-            msg: data,
-            name: socket.name
-        });
+  socket.on("chat message", function(data) {
+    io.emit("chat message", {
+      msg: data,
+      name: socket.name
     });
-    socket.on('disconnect', function (data) {
-        io.emit('leave', {
-            name: socket.name
-        });
+  });
+
+  socket.on("disconnect", function(data) {
+    io.emit("leave", {
+      name: socket.name
     });
+  });
 });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
-})
+http.listen(3000, function() {
+  console.log("listening on *:3000");
+});
